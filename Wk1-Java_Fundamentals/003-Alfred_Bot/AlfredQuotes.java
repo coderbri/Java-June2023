f0710a7a87be6fd9d3b298602e0e4a5f7ccafc18
coderbri <coderbri23@gmail.com>
2024-01-31 02:54:12 -0500

    wip project #003

--- Wk1-Java_Fundamentals/003-Alfred_Bot/AlfredQuotes.java
+++ Wk1-Java_Fundamentals/003-Alfred_Bot/AlfredQuotes.java
@@ -1,40 +1,90 @@
-import java.util.Date; // We'll be using Date from this library to request current time
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
 public class AlfredQuotes {
     
-    // Note: These greetings are not printed to the console,
-    //       but returned as a String fo use in the testing file.
-    
-    public String basicGreeting() { // eaxmple method
-        return "Hello, lovely to see you. How are you?";
+    /* Note: These greetings are not printed to the console,
+        but returned as a String fo use in the testing file. */
+    private String basicGreeting() {
+        return "\nHello, lovely to see you. How are you?";
     }
+    
+    /* Guest Greeting
+    ? Returns a greeting that includes the guest's name.
+        Input: String name
+        Return: String // ex: "Hello, Beth Cane. Lovely to see you."
+    */
+    public String guestGreeting() {
+        return basicGreeting();
+    }
+    
+    /* 忍者Ninja Bonus: Guest Greeting with Time
+    ? Returns a greeting including the guest's name as well as the day period.
+    ! Note: It must have the same method name but will have a different method signature.
+        Inputs: String name, String dayPeriod ("morning", "afternoon" or "evening")
+        Return Type: String ex: "Good evening, Beth Kane. Lovely to see you."
+    */
+    public String guestGreeting(String dayPeriod, String name) {
+        return String.format("Good %s, %s. How can I assist you today?", dayPeriod, name);
+    }
+    
+    /* 先生Sensei Bonus: Guest Greeting with Time Pt2
+    ? Returns greeting including guest's name and the ACTUAL time of day.
+    Instead of a string for the day period, write the overloaded method with
+        no parameters, and use a Calendar object to determine the day period.
+    */
+    public String guestGreeting(String name) {
+        /* The following is depracated:
+            Date currenDate = new Date();
+            int currentHour = currenDate.getHours();
+        So java.util.Calendar is used. */
+        Calendar calendar = Calendar.getInstance();
+        int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
+        
+        String dayPeriod;
+        if ( currentHour >= 0 && currentHour < 12 ) {
+            dayPeriod = "morning";
+        } else if ( currentHour >= 12 && currentHour < 18 ) {
+            dayPeriod = "afternoon";
+        } else {
+            dayPeriod = "evening";
+        }
+        
+        return String.format("Good %s, %s. How can I assist you today?", dayPeriod, name);
+    }
+    
+    /* Date Announcement Greeting
+    ? Returns a polite announcement of the current date and time.
+        Input: None
+        Return: String // ex: "It is currently Wed Aug 11 16:34:59 PDT 2022"
+    */
+    public String dateAnnouncement() {
+        return String.format("It is currently %s.\n", new Date());
+    }
+    public String dateAnnouncementSimplified() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("EEEE, MMMM dd, yyyy 'and' hh:mm a z");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("EST"));
+        return String.format("It is currently %s.\n", dateFormat.format( new Date() ));
+    }
+    
+    
+    
     
     /*
-    Todo NINJA BONUS: See the specs to overload the guestGreeting method
-        Using Scanner in the Test file, I'm practicing overloading methods
-        and passing variables so Alfred knows how to address the guest. */
-    public String guestGreeting(String name) {
-        return "Hello, " + name + "! How can I assist you today?";
-    }
+        TODO Respond before Alexis
+        ? AlfredBot will periodically process audio (Alfred is listening).
+        ? Write a method that accepts any String of conversation and responds appropiately.
+        Specifications:
+            If "Alexis" appears in the conversation (in the String), return a snarky response, such as "Right away, ma'am/sir. She certainly isn't sophisticated enough for that."
+            If "Alfred" is in the conversation return an obliging response. For example, "At your service. As you wish, naturally."
+            If neither name is found, return an appropiate response, for instance, "Right. And with that I shall retire."
+        Input: String (Any Phrase)
+            Return Type: String
+            Output: A response (String)
+    */
     
-    /* Inputs: String name, String — assume "morning", "afternoon", or "evening".
-        Return Type: String
-        Output: Returns a greeting Alfred might say,
-                That includes the person's name in the greeting. */
-    public String guestGreeting(String name, String dayPeriod) {
-        return String.format("Good %s, " + name + ". Lovely to see you.", dayPeriod, name);
-    }
-    
-    /* Inputs: None
-        Return Type: String
-        Output: Returns an announcement of the current date. */
-    public String dateAnnouncement() {
-        return String.format("It is currently %s", new Date());
-    }
-    
-    /* Input: String (Any Phrase)
-        Return Type: String
-        Output: A response (String) */
     public String respondBeforeAlexis(String conversation) {
         
         // the -1 is to test if whatever is in the indexOf exists
--- Wk1-Java_Fundamentals/003-Alfred_Bot/AlfredTest.java
+++ Wk1-Java_Fundamentals/003-Alfred_Bot/AlfredTest.java
@@ -1,43 +1,69 @@
 import java.util.Scanner;
 
 public class AlfredTest {
-    /* This main method will always be the launch point for a Java application
-        For now, we are using the main to test all our
-        AlfredQuotes methods.
-    */
-    
     public static void main(String[] args) {
         
-        // * Setting up Scanner to expect User Input
-        Scanner scanner = new Scanner(System.in);
-        
-        // Testing out Scanner
-        System.out.println("To whom do I have the honor of assisting?");
-        String name = scanner.nextLine();
-        
-        // Todo: Make an instance of AlfredQuotes to access all its methods.
+        // Making an instance of AlfredQuotes to access all its methods.
         AlfredQuotes alfredBot = new AlfredQuotes();
         
-        /*  Below alfredBot.guestGreeting() is using the variable, name, 
-            that has the scanner and is being shared with the method */
-        String greeting = alfredBot.guestGreeting(name);
+        // * Guest Greeting Test
+        String greeting = alfredBot.guestGreeting();
         // System.out.println(greeting);
+        
+        String guestName = System.console().readLine("\nTo whom do I have the honor of assisting? ");
+        String greetingGuestwDayPeriod = alfredBot.guestGreeting("morning", guestName);
+        String greetingGuest = alfredBot.guestGreeting(guestName);
+        // System.out.println(greetingGuestwDayPeriod);
+        System.out.println(greetingGuest);
+        
+        
+        
+        /* Setting up Scanner to expect User Input
+            Scanner scanner = new Scanner(System.in);
+            ? Testing out Scanner
+            System.out.println("To whom do I have the honor of assisting?");
+            String name = scanner.nextLine();
+        */
+        // * Date Announcement
+        String askDateFormat = System.console().readLine("\nHow would you like to know the date? (regular/simplified) ");
+        
+        String announceDate;
+        switch( askDateFormat.trim().toLowerCase() ) {
+            case "simplified":
+                announceDate = alfredBot.dateAnnouncementSimplified();
+                break;
+            case "regular":
+                announceDate = alfredBot.dateAnnouncement();
+                break;
+            default:
+                announceDate = "Invalid choice. Please choose 'regular' or 'simplified'.\n";
+                break;
+        }
+        /* Alternative: If-Else Statement
+        if ( "simplified".equalsIgnoreCase(askDateFormat.trim() )) {
+            announceDate = alfredBot.dateAnnouncementSimplified();
+        } else {
+            announceDate = alfredBot.dateAnnouncement();
+        } */
+        System.out.println(announceDate);
+        
         
         
         
         // Todo: Make some test greetings, providing any necessary data
-        String testGreeting = alfredBot.basicGreeting();
-        String testGuestGreeting = alfredBot.guestGreeting( name, "evening" );
-        String testDateAnnouncement = alfredBot.dateAnnouncement();
-        String alexisTest = alfredBot.respondBeforeAlexis( "Alexis! Play some low-fi beats." );
-        String alfredTest = alfredBot.respondBeforeAlexis( "I can't find my yo-yo. Maybe Alfred will know where it is." );
-        String notRelevantTest = alfredBot.respondBeforeAlexis( "Maybe that's what Batman is about. Not winning. But failing.." );
+        // String testGreeting = alfredBot.basicGreeting();
+        // String testGuestGreeting = alfredBot.guestGreeting( name, "evening" );
+        // String testDateAnnouncement = alfredBot.dateAnnouncement();
+        // String alexisTest = alfredBot.respondBeforeAlexis( "Alexis! Play some low-fi beats." );
+        // String alfredTest = alfredBot.respondBeforeAlexis( "I can't find my yo-yo. Maybe Alfred will know where it is." );
+        // String notRelevantTest = alfredBot.respondBeforeAlexis( "Maybe that's what Batman is about. Not winning. But failing.." );
 
         // * Print Test
         // System.out.println(testGreeting);
         
-        System.out.println(testGuestGreeting);
-        System.out.println(testDateAnnouncement);
+        // System.out.println(testGuestGreeting);
+        // System.out.println(testDateAnnouncement);
+        
         // System.out.println(alexisTest);
         // System.out.println(alfredTest);
         // System.out.println(notRelevantTest);
