9ce57c212dfa54c31e4edf9c0e5ebbcd9479db18
coderbri <coderbri23@gmail.com>
2024-03-08 01:26:28 -0500

    updated MainController.java
    added more test cases of the @PathVariable annotation

--- Wk3-Java_Spring/Lecture-Code/D7-Spring_Intro/FirstSpring/src/main/java/com/weekThree/firstSpring/controllers/MainController.java
+++ Wk3-Java_Spring/Lecture-Code/D7-Spring_Intro/FirstSpring/src/main/java/com/weekThree/firstSpring/controllers/MainController.java
@@ -2,16 +2,48 @@ package com.weekThree.firstSpring.controllers;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
 	
+	// ? Requests can be explicitly stated as well
+	// localhost:8080/test
+	@RequestMapping(value="/test", method=RequestMethod.GET)
+	public String test() {
+		return "This is a test!";
+	}
+	
 	// localhost:8080/hello/jane
 	@RequestMapping("/hello/{name}")
-	public String hello( @PathVariable("name") String name) {
+	public String hello( @PathVariable("name") String name ) {
+		System.out.println("\n============================\n"
+				+ "Variable printed to console: " + name);
 		return "<h1>你好，" + name +"。</h1>";
+	}
+	
+	// localhost:8080/apple/7
+	@RequestMapping("/test/{number}")
+	public String variableTest( @PathVariable("number") Long incomingNum ) {
+		String incomingWord = "String Repeat Test";
+		System.out.printf("\n============================\n" + "Variables: %s x %s", incomingWord, incomingNum);
+		String output = "";
+		for (int i = 0; i < incomingNum; i++) {
+			output += incomingWord;
+		}
+		return output;
+	}
+	
+	// localhost:8080/test/apple/7
+	@RequestMapping("/test/{word}/{number}")
+	public String repeatString(
+			@PathVariable("word") String incomingWord,
+			@PathVariable("number") Integer incomingNum
+			) {
+		System.out.printf("\n============================\n" + "Variables: %s x %s", incomingWord, incomingNum);
+		return incomingWord.repeat(incomingNum);
 	}
 	
 	// localhost:8080/hello?firstName=Jane&lastName=Doe
